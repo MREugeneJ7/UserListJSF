df52b0b626d07cd848d2f0bdb56bc4ea01cf7ec6
mreugenej7 <mreugenej7@mreugenej7-HP-Pavilion-Gaming-Laptop-16-a0xxx>
2022-10-05 12:40:22 +0100

    Adding list with delete and filter functionalities

---
+++ src/main/java/com/eugene/userlist/controller/UserListJSFController.java
@@ -0,0 +1,44 @@
+package com.eugene.userlist.controller;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import com.eugene.userlist.domain.Filter;
+import com.eugene.userlist.domain.User;
+import com.eugene.userlist.service.UserService;
+
+@SessionScope
+@Component (value = "userList")
+public class UserListJSFController {
+    @Autowired
+    private UserService service;
+
+    private List<User> users;
+    private Filter filter;
+
+    @PostConstruct
+    public void loadData() {
+        filter = new Filter();
+        users = service.getUserList( filter.getDateFrom().toString(), filter.getDateTo().toString(), filter.getEmail());
+    }
+    
+    public List<User> getUsers() {
+        users = service.getUserList( filter.getDateFrom().toString(), filter.getDateTo().toString(), filter.getEmail());
+        return users;
+    }
+    
+    public void remove(User user) {
+        service.delete(user);
+        users = service.getUserList( filter.getDateFrom().toString(), filter.getDateTo().toString(), filter.getEmail());
+    }
+
+	public Filter getFilter() {
+		return filter;
+	}
+   
+}
